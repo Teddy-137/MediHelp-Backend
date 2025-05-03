4333228ee07bb48663c660f0dd594f3dcd182d55
Teddy <tewodrosanteneh@outlook.com>
2025-05-03 13:09:23 +0300

    feat: Add superuser creation to build script if it doesn't exist

--- build.sh
+++ build.sh
@@ -10,3 +10,18 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
+
+# Create superuser if it doesn't exist
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+email = "${DJANGO_SUPERUSER_EMAIL}"
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        first_name="${DJANGO_SUPERUSER_FIRST_NAME}",
+        last_name="${DJANGO_SUPERUSER_LAST_NAME}",
+        phone="${DJANGO_SUPERUSER_PHONE}",
+        password="${DJANGO_SUPERUSER_PASSWORD}"
+    )
+EOF
